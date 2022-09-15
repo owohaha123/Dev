4931aadc37c4700f7d5b9c28fb795ec0e5833784
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-15 16:48:05 +0900

    MainClass (2)

---
+++ web_work/JAVA/java_3day/src/MainClass.java
@@ -0,0 +1,8 @@
+import controller.ControllerClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ControllerClass cc = new ControllerClass();
+        cc.run();
+    }
+}
