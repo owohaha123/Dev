9ad55f47f63787aad5c0f48510e0c24de4eef9ed
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-15 16:47:52 +0900

    DataDto

---
+++ web_work/JAVA/java_3day/src/dto/DataDto.java
@@ -0,0 +1,33 @@
+package dto;
+
+public class DataDto {
+    private int number;
+    private String str;
+    private float fnumber;
+
+    public float getFnumber() {
+        return fnumber;
+    }
+
+    public void setFnumber(float fnumber) {
+        this.fnumber = fnumber;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+
+}
