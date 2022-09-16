2169f22b0a17dec96c3de401d42da95ae448b328
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-16 16:06:39 +0900

    실습)연락처 관리 프로그램 _ dto
    - getter & setter

---
+++ web_work/JAVA/phonebook/src/com/dto/PhoneInfo.java
@@ -0,0 +1,40 @@
+package com.dto;
+
+public class PhoneInfo {
+    private String name;
+    private String phone;
+    private String birth;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
