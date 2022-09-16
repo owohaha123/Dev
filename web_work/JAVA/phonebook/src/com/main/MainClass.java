91aea1970c236399f190a8560afcf932ddfceb82
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-16 16:05:13 +0900

    실습)연락처 관리 프로그램 _ main
    - 구현 대상
      정보 : 이름, 연락처, 생일, 나이
      기능 : 입력, 출력, 검색, 수정, 삭제
    
    - 순서
       1. 패키지 생성 ( dto / main / controller / view )
       2. 각 패키지 안에 class 생성
         > dto 패키지에 PhoneInfo
         > main 패키지에 MainClass
         > controller 패키지에 PhoneController
         > view 패키지에 InOutClass
       3. PhoneController에 기능 구현

---
+++ web_work/JAVA/phonebook/src/com/main/MainClass.java
@@ -0,0 +1,10 @@
+package com.main;
+
+import com.controller.PhoneController;
+
+public class MainClass {
+    public static void main(String[] args) {
+        PhoneController pc = new PhoneController();
+        pc.run();
+    }
+}
