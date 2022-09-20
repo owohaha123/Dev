18219cd4050dc1a03fe11d2f9f9933ab1fffbb33
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-20 13:31:13 +0900

    generate로 constructor 생성
    - generate 로 constructor 생성

---
+++ web_work/JAVA/java_6day/src/com/dto/PhoneInfo.java
@@ -0,0 +1,15 @@
+package com.dto;
+
+public class PhoneInfo {
+    private String name;
+    private String phone;
+    private String birth;
+    private int age;
+
+    public PhoneInfo(String name, String phone, String birth, int age) {
+        this.name = name;
+        this.phone = phone;
+        this.birth = birth;
+        this.age = age;
+    }
+}
