e6e1b39d1d36cb42c01b52fb07d8458e8ffb4b9c
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-20 15:55:49 +0900

    PointTest
    - 오버라이딩
    - 부모클래스와 자식클래스에 있는 변수명이 동일해도 다른 변수로 취급

---
+++ web_work/JAVA/java_6day/src/PointTest.java
@@ -0,0 +1,84 @@
+public class PointTest {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point3d p31 = new Point3d();
+        p1.x = 10;
+        p1.y = 20;
+        p1.color = "red";
+        p1.showPoint();
+        //p1.z = 22;
+
+        p31.x = 30;
+        p31.y = 50;
+        p31.z = 25;
+        p31.color = "blue";
+        p31.showPoint();
+        System.out.println(p31);
+
+        System.out.println(p1.pattern); // 어떤 모양
+        p1.pattern = "물결";
+        System.out.println(p1.pattern);  // 물결
+        System.out.println(p31.pattern); // null
+        p31.pattern = "구름";
+        System.out.println(p31.pattern); // 구름
+        p31.showPattern();
+    }
+}
+
+class Point { // 2D 그래프용 점 객체
+    int x;
+    int y;
+    String color;
+    String pattern = "어떤 모양";
+
+    public Point(){}
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    void showPoint(){
+        System.out.println("x : " + x + ", y : " + y + ", color : " + color);
+    }
+    @Override
+    public String toString() {
+        return "x :" + x + " y :" + y ;
+    }
+}
+
+class Point3d extends Point{ // 3D 그래프용 점 객체
+    int z;
+    String pattern; // 부모클래스와 자식클래스에 있는 변수명이 동일해도 다른 변수로 취급!
+
+    public Point3d(){
+        //super(); // 생략 가능
+    }
+
+    public Point3d(int x, int y, int z){
+        super(x, y); // x, y는 부모클래스에서 초기화 하고
+        this.z = z;  // 부모클래스에 없는 변수만 처리
+        // 이 때, super 는 맨 첫 줄에 사용 (뒤에 쓰이면 x)
+    }
+
+    @Override // Annotation (어노테이션)
+    void showPoint(){
+        System.out.println("x : " + x + ", y : " + y + ", z : " + z +", color : " + color);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", z :" + z;
+    }
+
+    public void showPattern(){
+        System.out.println("부모의 pattern : " + super.pattern); // 구분을 위해 super. 사용
+        System.out.println("pattern : " + pattern);
+    }
+}
+
+//class Point3d { // 3D 그래프용 점 객체
+//    int x;
+//    int y;
+//    int z;
+//    String color;
+//}
