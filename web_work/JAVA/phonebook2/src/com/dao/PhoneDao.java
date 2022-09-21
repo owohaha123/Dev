3d57d6c89d83775bbe390f3e6e6142a7427eb5de
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-21 15:14:00 +0900

    수정
    - 각 list 의 마지막 번호를 구하는 메소드 추가

--- web_work/JAVA/phonebook2/src/com/dao/PhoneDao.java
+++ web_work/JAVA/phonebook2/src/com/dao/PhoneDao.java
@@ -40,7 +40,41 @@ public class PhoneDao {
                 break;
             case 3:
                 size = pComList.size();
+                break;
         }
         return size; // 중요!
     }// pListSize end
+
+    // 각 list 의 마지막 번호를 구하는 메소드
+    public int getLastNum(int cate){
+        int lno = 0;
+        switch (cate) {
+            case 1:
+                // 마지막 연락처의 일련번호(no) 구하기
+                // 0번 부터 시작하기에 -1
+                int n = pList.size();
+                PhoneInfo p = pList.get(n-1);
+                lno = p.getNo();
+                //lno = pList.get(pList.size() -1).getNo();
+                break;
+            case 2:
+                lno = pSchList.get(pSchList.size() -1).getNo();
+                break;
+            case 3:
+                lno = pComList.get(pComList.size() -1).getNo();
+                break;
+        }
+        return lno;
+    } // getLastNum end
+
+    public PhoneInfo getPhoneInfo(int idx){
+        return pList.get(idx);
+    }
+    public PhoneSchInfo getSchInfo(int idx){
+        return pSchList.get(idx);
+    }
+    public PhoneComInfo getComInfo(int idx){
+        return pComList.get(idx);
+    }
+
 }// dao class end
