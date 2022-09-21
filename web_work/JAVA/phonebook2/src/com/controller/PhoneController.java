179e11014e6ebffde7208e6154554af6c22afab8
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-21 15:15:09 +0900

    수정
    * 추가
    - 출력 메소드 outputData()
    - 전체출력 메소드 outputTotal(
    - 회사입력 메소드 inputCompany()
    - 학교입력 메소드 inputSchool()

--- web_work/JAVA/phonebook2/src/com/controller/PhoneController.java
+++ web_work/JAVA/phonebook2/src/com/controller/PhoneController.java
@@ -1,14 +1,16 @@
 package com.controller;
 
 import com.dao.PhoneDao;
+import com.dto.PhoneComInfo;
 import com.dto.PhoneInfo;
+import com.dto.PhoneSchInfo;
 import com.view.InOutClass;
 
 public class PhoneController {
     private InOutClass io = new InOutClass();
     private PhoneDao pDao = new PhoneDao();
 
-    // 전체 제어 메소드
+// --------------------------- 전체 제어 메소드 run() --------------------------------
     public void run(){
         int menu = -1;
         // 타이틀 출력
@@ -28,6 +30,7 @@ public class PhoneController {
                     inputData();
                     break;
                 case 2:
+                    outputData();
                     break;
                 case 3:
                     break;
@@ -41,6 +44,70 @@ public class PhoneController {
         }
     }
 
+// ----------------------- 출력 메소드 outputData() -----------------------
+    private void outputData() {
+        // 연락처가 모두 비어있는 경우
+        if(pDao.pListSize(1) == 0
+            && pDao.pListSize(2) == 0
+            && pDao.pListSize(3) == 0){
+            io.twoPrint("저장된 연락처가 없습니다 \n");
+            return;
+        }
+
+        int submenu = -1;
+        // 서브타이틀 출력
+        io.twoPrint("<< 연락처 출력 >>");
+        io.twoPrint("----------------");
+
+        while (true) {
+            // 출력 하위 메뉴
+            io.twoPrint("1. 전체 출력");
+            io.twoPrint("2. 구분 출력");
+            io.twoPrint("0. 돌아가기");
+            submenu = io.inNum("입력 > ");
+
+            if(submenu == 0){
+                io.twoPrint("메인메뉴로 돌아갑니다. \n");
+                break;
+            }
+            switch (submenu){
+                case 1: // 1. 전체 출력
+                    outputTotal();
+                    break;
+                case 2: // 2. 구분 출력 (일반, 학교, 회사)
+
+                    break;
+                default:
+                    io.twoPrint("0~2번까지 입력하세요");
+            }
+        }
+    }
+
+// ----------------------- 전체출력 메소드 outputTotal()
+    private void outputTotal() {
+        // 전체 출력 메소드
+        io.twoPrint("< 전체출력 >");
+        io.twoPrint("-----------");
+        // 일반 출력 > 학교 출력 > 회사 출력
+        for(int i = 0; i < pDao.pListSize(1); i++){
+            io.twoPrint("<<<일반 연락처>>>");
+            io.twoPrint(pDao.getPhoneInfo(i).toString());
+            io.twoPrint("----------------");
+        }
+        for(int j = 0; j < pDao.pListSize(2); j++){
+            io.twoPrint("<<<학교 연락처>>>");
+            io.twoPrint(pDao.getSchInfo(j).toString());
+            io.twoPrint("----------------");
+        }
+        for(int k = 0; k < pDao.pListSize(3); k++){
+            io.twoPrint("<<<회사 연락처>>>");
+            io.twoPrint(pDao.getComInfo(k).toString());
+            io.twoPrint("----------------");
+        }
+        io.twoPrint("출력 완료!!! \n");
+    }
+
+// ----------------------- 입력 메소드 inputData() -----------------------
     private void inputData() {
         int submenu = -1;
         // 서브타이틀 출력
@@ -60,8 +127,10 @@ public class PhoneController {
                     inputNormal();
                     break;
                 case 2:
+                    inputSchool();
                     break;
                 case 3:
+                    inputCompany();
                     break;
                 default:
                     io.twoPrint("0~3번까지 입력하세요");
@@ -70,6 +139,51 @@ public class PhoneController {
         }// while end
     }// inputData end
 
+// ----------------------- 회사입력 메소드 inputCompany()
+    private void inputCompany() {
+        int no = 0;
+        if(pDao.pListSize(3) == 0){
+            no = 1;
+        }else{
+            no = pDao.getLastNum(3) + 1;
+        }
+        PhoneComInfo pInfo = new PhoneComInfo();
+//        pInfo.setNo(no); // 일련번호 저장
+//        pInfo.setName(io.inStr("이름 : "));
+//        pInfo.setPhone(io.inStr("연락처 : "));
+//        pInfo.setBirth(io.inStr("생일 : "));
+//        pInfo.setAge(io.inNum("나이 : "));
+
+        pInfo.setCompany (io.inStr("회사 : "));
+        pInfo.setDept(io.inStr("부서 : "));
+        pInfo.setLevel(io.inStr("직급 : "));
+
+        pDao.inputData(pInfo);
+    }
+
+// ----------------------- 학교입력 메소드 inputSchool()
+    private void inputSchool() {
+        int no = 0;
+        if(pDao.pListSize(2) == 0){
+            no = 1;
+        }else{
+            no = pDao.getLastNum(2) + 1;
+        }
+        PhoneSchInfo pInfo = new PhoneSchInfo();
+//        pInfo.setNo(no); // 일련번호 저장
+//        pInfo.setName(io.inStr("이름 : "));
+//        pInfo.setPhone(io.inStr("연락처 : "));
+//        pInfo.setBirth(io.inStr("생일 : "));
+//        pInfo.setAge(io.inNum("나이 : "));
+
+        pInfo.setSname(io.inStr("학교 : "));
+        pInfo.setMajor(io.inStr("전공 : "));
+        pInfo.setYear(io.inNum("학년 : "));
+
+        pDao.inputData(pInfo);
+    }
+
+// ----------------------- 일반입력 메소드 inputNormal()
     private void inputNormal() {
         // 각 연락처(일반, 학교, 회사)별 일련번호
         int no = 0;
@@ -77,20 +191,21 @@ public class PhoneController {
             no = 1;
         }else{
             // 마지막 데이터의 번호를 구하고 +1 한다
-            no++; // 임시로 처리 (삭제 예정)
+            no = pDao.getLastNum(1) + 1;
         }
 
         PhoneInfo pInfo = new PhoneInfo();
-        pInfo.setNo(no); // 일련번호 저장
-        pInfo.setName(io.inStr("이름 : "));
-        pInfo.setPhone(io.inStr("연락처 : "));
-        pInfo.setBirth(io.inStr("생일 : "));
-        pInfo.setAge(io.inNum("나이 : "));
+//        pInfo.setNo(no); // 일련번호 저장
+//        pInfo.setName(io.inStr("이름 : "));
+//        pInfo.setPhone(io.inStr("연락처 : "));
+//        pInfo.setBirth(io.inStr("생일 : "));
+//        pInfo.setAge(io.inNum("나이 : "));
 
         pDao.inputData(pInfo);
     }
 
-    // 서브메뉴 출력 메소드
+// -------------------------- 메뉴 출력 메소드  -------------------------------
+// ----------------------- 서브메뉴 출력 메소드 subMenuShow()
     public void subMenuShow(){
         io.twoPrint("1. 일반 연락처");
         io.twoPrint("2. 학교 연락처");
@@ -99,7 +214,7 @@ public class PhoneController {
     }
 
 
-    // 메뉴출력 메소드
+// ----------------------- 메뉴출력 메소드 menuShow()
     private void menuShow(){
         io.twoPrint("1. 연락처 입력");
         io.twoPrint("2. 연락처 출력");
