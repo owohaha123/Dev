a28ac4f971c6aba1642b06191efc346852d54e9b
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-09-23 14:08:05 +0900

    Update CalendarTest.java

--- web_work/JAVA/java_9day/src/datetime/CalendarTest.java
+++ web_work/JAVA/java_9day/src/datetime/CalendarTest.java
@@ -25,6 +25,18 @@ public class CalendarTest {
         int lastDayOfMonth = cal.getActualMaximum(Calendar.DATE);
         System.out.println("이번 달의 마지막 날 : " + lastDayOfMonth);
 
+        // 날짜 지정
+        Calendar cal2 = Calendar.getInstance();
+        // 연, 월, 일 지정
+        cal2.set(Calendar.YEAR, 2022);
+        cal2.set(Calendar.MONTH, 7); // 실제 월 -1 값 입력
+        cal2.set(Calendar.DATE, 1); // 보통 기본은 1일로 설정
+        System.out.println(cal2);
 
+        // 시작일의 요일(1: 일 7: 토)
+        int startDay = cal2.get(Calendar.DAY_OF_WEEK);
+        System.out.println(startDay);
+        int lastDay = cal2.getActualMaximum(Calendar.DATE);
+        System.out.println(lastDay);
     }
 }
