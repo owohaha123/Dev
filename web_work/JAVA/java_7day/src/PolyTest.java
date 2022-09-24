8e4b825dfcae75659eee64cb390c3734e4b0f0f8
김윤주 <owohaha123@gmail.com>
2022-09-24 18:53:11 +0900

    Update PolyTest.java

--- web_work/JAVA/java_7day/src/PolyTest.java
+++ web_work/JAVA/java_7day/src/PolyTest.java
@@ -10,24 +10,24 @@ public class PolyTest {
 
         p5.a = 10;
         p5.b = 20;
-        //p5.c = 30;
+        //p5.c = 30; // 부모로 형변환 됐기에 사용 불가
 
         // down casting
-        //Child5 cc5 = (Child5)p5; // 강제형변환
-        //Child5 cc6 = (Child5)p7; // X
-        //Child6 cc7 = (Child6)p6;
+        //Child5 cc5 = (Child5)p5; // O. 강제형변환
+        //Child5 cc6 = (Child5)p7; // X. 부모클래스로 만든 인스턴스(p7)는 자식 클래스로 만든 참조변수(cc6)에 저장할 수 없다
+        //Child6 cc7 = (Child6)p6; // O. 강제형변환
 
-        Parent5 p = new Child6();
+        Parent5 p = new Child6(); // 상속받은 관계라서 가능
         if(p instanceof Child5){
             Child5 cc5 = (Child5)p;
-            System.out.println("Child5로 만든 인스턴스!");
+            System.out.println("Child5로 만든 인스턴스!");// X
             cc5.c = 300;
         }else{
             Child6 cc7 = (Child6)p;
             System.out.println("Child6로 만든 인스턴스!");
             cc7.d = 400;
         }
-    }
+    } // 결과 : Child6로 만든 인스턴스!
 }
 
 class Parent5 {
