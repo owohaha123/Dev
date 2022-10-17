f63a7d49a45acd1c2ba18184cc72b0063f09b3f5
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-10-17 16:18:31 +0900

    Spring_1day
    jsp 02

---
+++ web_work/spring-work/jsp02/src/main/java/com/jsframe/jsp02/Jsp02Application.java
@@ -0,0 +1,13 @@
+package com.jsframe.jsp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsp02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsp02Application.class, args);
+    }
+
+}
---
+++ web_work/spring-work/jsp02/src/test/java/com/jsframe/jsp02/Jsp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsframe.jsp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsp02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
