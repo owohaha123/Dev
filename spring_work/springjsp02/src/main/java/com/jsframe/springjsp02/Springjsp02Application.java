ca7d395a1b56ed13b2a5904244eb53e062cfea4d
김윤주 <owohaha123@gmail.com>
2022-10-19 16:43:58 +0900

    Spring_3day

---
+++ spring_work/springjsp02/src/main/java/com/jsframe/springjsp02/Springjsp02Application.java
@@ -0,0 +1,13 @@
+package com.jsframe.springjsp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjsp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjsp02Application.class, args);
+	}
+
+}
---
+++ spring_work/springjsp02/src/test/java/com/jsframe/springjsp02/Springjsp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsframe.springjsp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjsp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
