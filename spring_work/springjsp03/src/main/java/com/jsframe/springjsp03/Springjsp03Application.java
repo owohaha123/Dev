5e2afd4a65fcacab9b7e179e0c2a9b01299e4480
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-10-20 16:46:46 +0900

    Spring_4day

---
+++ spring_work/springjsp03/src/main/java/com/jsframe/springjsp03/Springjsp03Application.java
@@ -0,0 +1,13 @@
+package com.jsframe.springjsp03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjsp03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springjsp03Application.class, args);
+    }
+
+}
