2b16bc035c0863093331d56948d86498394630f9
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-10-25 16:06:52 +0900

    JQuery_1day

---
+++ spring_work/jqueryspr_7day/src/main/java/com/jsframe/jqueryspr/JquerysprApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.jqueryspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JquerysprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JquerysprApplication.class, args);
+    }
+
+}
---
+++ spring_work/jqueryspr_7day/src/test/java/com/jsframe/jqueryspr/JquerysprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsframe.jqueryspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JquerysprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
