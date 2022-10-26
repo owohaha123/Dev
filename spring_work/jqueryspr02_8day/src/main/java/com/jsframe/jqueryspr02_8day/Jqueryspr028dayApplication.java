3ea7f8e56c746ef350bfaf569e01e75f20211403
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-10-26 14:53:54 +0900

    JQuery_2day

---
+++ spring_work/jqueryspr02_8day/src/main/java/com/jsframe/jqueryspr02_8day/Jqueryspr028dayApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.jqueryspr02_8day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jqueryspr028dayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jqueryspr028dayApplication.class, args);
+    }
+
+}
---
+++ spring_work/jqueryspr02_8day/src/test/java/com/jsframe/jqueryspr02_8day/Jqueryspr028dayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsframe.jqueryspr02_8day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jqueryspr028dayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
