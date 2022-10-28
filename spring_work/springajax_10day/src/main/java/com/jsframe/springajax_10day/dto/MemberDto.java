3428a3d9575082fe5251de022d8750c02a5f6896
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-10-28 16:50:16 +0900

    Dto

---
+++ spring_work/springajax_10day/src/main/java/com/jsframe/springajax_10day/dto/MemberDto.java
@@ -0,0 +1,10 @@
+package com.jsframe.springajax_10day.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private String m_id;
+    private String m_pwd;
+    private String m_name;
+}
