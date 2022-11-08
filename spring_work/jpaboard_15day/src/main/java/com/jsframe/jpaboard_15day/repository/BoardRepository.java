eace0610a2b5aea48a7bfb1a4bbccc6835d736ec
owohaha123 <104568039+owohaha123@users.noreply.github.com>
2022-11-08 16:44:56 +0900

    수정

--- spring_work/jpaboard_15day/src/main/java/com/jsframe/jpaboard_15day/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jsframe.jpaboard_15day.repository;
-
-import com.jsframe.jpaboard_15day.entity.Board;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BoardRepository extends CrudRepository<Board, Long> {
-}
