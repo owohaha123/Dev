e0fffdd436dbb65cc1a83e8ca41a02e90b2cecc4
owohaha123 <owohaha123@gmail.com>
2022-11-21 15:57:40 +0900

    react_2day (ES6 문법)
    9. import와 export (외부모듈사용)

---
+++ react_work/ES6-js/component.js
@@ -0,0 +1,7 @@
+// 배열 내보내기
+ let months = ['Jan','Feb','Mar','Apr','May','Jun'
+                    ,'Jul','Aug','Sep','Oct','Nov','Dec'];
+
+ const NOW_YEAR = 2022;  
+
+export {months , NOW_YEAR};
