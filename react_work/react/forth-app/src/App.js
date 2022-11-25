833a031fecaea4fe163f5c7689cadbf4810ff6d5
owohaha123 <owohaha123@gmail.com>
2022-11-25 14:55:10 +0900

    Create App.js

---
+++ react_work/react/forth-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComp from './SassComp';
+
+function App() {
+  return (
+    <div className="App">
+      <SassComp />
+    </div>
+  );
+}
+
+export default App;
