1ae14b7e90e47466a2b78c03029497b976da91f9
owohaha123 <owohaha123@gmail.com>
2022-11-25 14:52:40 +0900

    sass

---
+++ react_work/react/forth-app/src/SassComp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './SassComp.scss';
+
+const SassComp = () => {
+    return (
+        <div className="SassComp">
+            <div className="box red">빨간 박스</div>
+            <div className="box orange">오렌지 박스</div>
+        </div>
+    );
+};
+
+export default SassComp;
