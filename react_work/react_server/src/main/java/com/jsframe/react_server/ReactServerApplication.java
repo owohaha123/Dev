3106c2fd6ce3386e5fe7dee5f5f51bd171cec0a6
김윤주 <owohaha123@gmail.com>
2022-11-29 23:19:55 +0900

    react_8day(2)
    - server

---
+++ react_work/react_server/src/main/java/com/jsframe/react_server/ReactServerApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.react_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactServerApplication.class, args);
+	}
+
+}
